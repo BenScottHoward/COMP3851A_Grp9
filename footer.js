ba1572b825799854070415674e0aacb27250c167
damanjitgill <70557359+damanjitgill@users.noreply.github.com>
2021-03-25 21:06:05 +1100

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,9 @@
+
+
+function Footer() {
+    return (
+        <footer className="App-footer">&copy; 2021 University of Newcastle</footer>
+    )
+}
+
+export default Footer;
---
+++ header.js
@@ -0,0 +1,9 @@
+
+
+function Header() {
+    return (
+        <header className="App-header">Enter your degree and Major</header>
+    )
+}
+
+export default Header;
